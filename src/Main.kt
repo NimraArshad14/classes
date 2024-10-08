6c40cecfba0c4a2b823950ad5c4a155fe31e7e3a
Nimramayo <manimra840@gmail.com>
2024-10-08 15:47:10 +0500

    classes

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,7 @@
 fun main() {
     println("Hello world")
   val person = Person("nimra",7,60)
-    val student = Student("laiba",5,50)
+    val student = Student("laiba",5,50,2024)
 
 
 
@@ -17,19 +17,24 @@ class Person (name: String){
    }
     constructor(name:String,age:Int):this(name){}
     constructor(name:String,age:Int,marks:Int):this(name,age){}
+
 }
 //multiple constructor using properties method
 class Student (name: String) {
     var name: String = "laiba"
     var age: Int = 5
-
-
+    var marks : Int = 985
+    var year : Int = 2024
     init {
         println("laiba")
     }
 
     init {
         println(5 + 7)
+        println(985)
+    }
+    init {
+        println(2024)
     }
 
     constructor(name: String, age: Int) : this(name) {
@@ -42,6 +47,13 @@ class Student (name: String) {
         this.name = name
         this.age = age
 
+
+    }
+    constructor(name: String, age: Int, marks: Int, year : Int) : this(name, age,marks) {
+        this.name = name
+        this.age = age
+        this.marks = marks
+        this.year = year
 
     }
 }
